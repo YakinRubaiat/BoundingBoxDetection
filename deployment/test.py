21256086592a093ab5baef75a39dd78d0ffc3475
Yakin Rubaiyat <yaken.rubaiat11@gmail.com>
2021-05-16 12:08:12 +0600

    Add load model

---
+++ deployment/test.py
@@ -0,0 +1,5 @@
+model1_path = './models/resnet50_csv_22.h5'
+model1 = models.load_model(model1_path, backbone_name='resnet50', convert=True, nms=False)
+
+model2_path = './models/resnet101_csv_21.h5'
+model2 = models.load_model(model2_path, backbone_name='resnet101', convert=True, nms=False)
